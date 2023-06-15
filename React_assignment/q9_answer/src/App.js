e45fcd82b65166716b0c31729d7132fac2fc5067
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-06-15 23:37:08 +0530

    context-api started

--- React_assignment/q9_answer/src/App.js
+++ React_assignment/q9_answer/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./navbar.js"
+import React,{ useState } from "react";
 function App() {
+
+  const theme=React.createContext();
+  const[dark,setDark]=useState('false');
+
+  const getTheme=()=>{
+    if(dark){
+      setDark('true');
+    }
+    else{
+      setDark('false');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <theme.Provider themeColor={dark}>
+  <Navbar/>
+  </theme.Provider>
+  <div>
+    <buttton onclick="getTheme">Change Theme</buttton>
+  </div>
+  
+  </>
   );
 }
 
---
+++ React_assignment/q9_answer/src/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar(){
+    
+    return(
+        <>
+        <div style={{display:"flex",justifyContent:"center",gap:"80px",marginTop:"15px"}}>
+            <div>HOME</div>
+            <div>ABOUT US</div>
+            <div>CONTACT US</div>
+            <div>LOGIN</div>
+        </div>
+        </>
+    );
+}
+
+export default NavBar;
