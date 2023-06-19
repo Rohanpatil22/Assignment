ad642eb31b5e487c57be49680d15dd01a2d4e153
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-06-19 23:02:32 +0530

    add and remove task functionality added

--- React_assignment/q11_answer/src/App.js
+++ React_assignment/q11_answer/src/App.js
@@ -1,11 +1,13 @@
 import {nanoid} from "nanoid";
 import {React,useState,useReducer} from "react";
 import reducer from "./reducer.js"
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrash} from "@fortawesome/free-solid-svg-icons"
 
  
 function App() {
  
-  const taskList=[{id:'1',todo:"Need to learn React"}];
+  const taskList=[{id:'1',todo:"Need to learn React"},{id:'2',todo:"Have to learn express js"}];
   const[task,setTask]=useState();
   const [state,dispatch]=useReducer(reducer,taskList)
   const handleOnchannge=(event)=>{
@@ -13,14 +15,23 @@ function App() {
   }
 
   const addTask=()=>{
-    const newTaskobj={
-      id:nanoid(),
-      todo:task
+    if(task!=="" && task!==undefined)
+    {
+      console.log(task);
+      const newTaskobj={
+        id:nanoid(),
+        todo:task
+      }
+      dispatch({
+          type:"addTask",
+          newtask:newTaskobj,
+        });
     }
-    dispatch({
-        type:"addTask",
-        newtask:newTaskobj,
-      });
+    else{
+      console.log(task);
+      alert("Please add Task");
+    }
+   
   }
 
   const removeTask=(num)=>{
@@ -32,14 +43,14 @@ function App() {
   }
   return (
     <>
-    <div>
-        <input type="text" onChange={handleOnchannge}/>
-        <button onClick={addTask}>ADD</button>
+    <div className="m-auto mt-10 w-2/5">
+        <input className="p-1 w-4/5 bg-neutral-500" type="text" onChange={handleOnchannge} placeholder="Enter task"/>
+        <button className="ml-4 bg-emerald-700 p-2 w-20  rounded-xl text-white mb-10" onClick={addTask}>ADD</button>
     </div>
     <div>
       {state.map((item)=>(
         // <div key={item.id}><input id={item.id} style={{border:"none"}} value={item.todo} readOnly/><button onClick={removeTask(item.id)}>remove</button>
-        <div key={item.id}>{item.todo}<button onClick={removeTask(item.id)}>remove</button>
+        <div className="text-lg text-white w-2/5 m-auto border-1 bg-cyan-800 mt-2 p-1 flex justify-between pr-6" key={item.id}>{item.todo}<button onClick={()=>removeTask(item.id)}><FontAwesomeIcon icon={faTrash}/></button>
         </div>
       ))
 
