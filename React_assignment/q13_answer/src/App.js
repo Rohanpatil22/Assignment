5fab09b8701c0adac7e09bc7609f14be758cb591
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-06-24 23:30:37 +0530

    calculator app completed

--- React_assignment/q13_answer/src/App.js
+++ React_assignment/q13_answer/src/App.js
@@ -5,58 +5,55 @@ function App() {
 
   const [userInp,setUserInp]=useState('');
   const[result,setResult]=useState(0);
-  const[currOps,setCurrOps]=useState('');
+  
 
   let operations=["+","-","/","*"];
 
   const getUserInp=(val)=>{
-    console.log(val);
-    let prevInp=userInp;
-    if(operations.includes(val))
-    {
-      setCurrOps(val);
-    }
     
-      setUserInp(prevInp+val);
-    let num=parseInt(val);
-   // console.log(num);
-    getResult(num);
-    
-    
-  }
+    setUserInp(userInp+val);
 
-  let lastData=0;
+    // let lastInp=userInp.charAt(userInp.length-1);
+
+    // if(!operations.includes(lastInp))
+    // {
+    //   let resCal=eval(userInp);
+    //   setResult(resCal);
+    // }  
+  }
   
-  const getResult=(data)=>{
-   lastData+=data;
-   
-   if(currOps!=='')
-   {
-   let total=parseInt(userInp);
-   console.log(total)
-   console.log(typeof(total))
-   console.log(total);
-    setResult(total);
+   const clearUserInp=()=>{
 
+    setUserInp("");
+    setResult(0);
    }
-   
+  
+   const getResult=()=>{
+    let lastInp=userInp.charAt(userInp.length-1);
+    if(!operations.includes(lastInp))
+    {
+      
+      let resCal=eval(userInp);
+      setResult(resCal);
+    }  
     
-  }
+   }
+  
   return (
    <>
    <div className="w-1/3 mt-28 m-auto">
      <div className="bg-neutral-900 p-4">
-       <div className="bg-neutral-700 text-5xl text-white outline-none w-full h-24 text-right rounded ">
+       <div className="bg-neutral-700 text-5xl text-white outline-none w-full h-28 text-right rounded ">
         <div>{userInp}</div>
-        <div>{result}</div></div>
+        <div className="mt-2">{result}</div></div>
        <div className="mt-3">
        <table className="w-full m-auto">
         <tbody>
            <tr>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp("")}>C</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">Del</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">%</button></td>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">/</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400" onClick={()=>clearUserInp("")}>C</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>setUserInp(userInp.slice(0,-1))} >Del</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('%')}>%</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('/')}>/</button></td>
             </tr>
             <tr>
               <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('7')}>7</button></td>
@@ -66,24 +63,24 @@ function App() {
             </tr>
 
             <tr>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400">4</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">5</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">6</button></td>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">-</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('4')}>4</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('5')}>5</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('6')}>6</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('-')}>-</button></td>
             </tr>
 
             <tr>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400 ">1</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">2</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">3</button></td>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">+</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('1')}>1</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('2')}>2</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('3')}>3</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getUserInp('+')}>+</button></td>
             </tr>
 
             <tr>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400 ">00</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">0</button></td>
-              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 ">.</button></td>
-              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400">=</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('00')}>00</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('0')}>0</button></td>
+              <td><button className="bg-neutral-700 text-white p-6  m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400 " onClick={()=>getUserInp('.')}>.</button></td>
+              <td><button className="bg-neutral-700 text-white p-6 m-1 text-3xl  w-24 rounded-lg hover:bg-stone-400" onClick={()=>getResult()} >=</button></td>
             </tr>
             </tbody>
        </table>
