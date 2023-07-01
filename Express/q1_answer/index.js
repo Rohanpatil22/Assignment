946ac257daa85b7cbc8ec07827b3e734e73ba61e
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-07-01 23:50:59 +0530

    express js q1 answer

--- Express/q1_answer/index.js
+++ Express/q1_answer/index.js
@@ -1,19 +1,22 @@
-// import mongoose from 'mongoose';
+
 const express = require('express');
 const mongoose= require('mongoose');
 
 
 const app=express();
 const PORT=3000;
-const db="mongodb+srv://rohanpatil1797:AdsDcwJHEfPiYps4@cluster0.tn9rfaf.mongodb.net/"
-app.listen(PORT, (error)=>{
+const db="mongodb+srv://rohanpatil1797:AdsDcwJHEfPiYps4@cluster0.tn9rfaf.mongodb.net/post_data"
+app.listen(PORT, async (error)=>{
 
     if(!error)
     {
         console.log("Server is successfully runnimg on PORT 3000");
        try{
-        mongoose.connect(db);
-        console.log("databse connected")
+       await mongoose.connect(db);
+        console.log("databse connected");
+
+        
+
        } 
        catch(err){
         console.log("Not connected to databse",err);
@@ -27,6 +30,29 @@ app.listen(PORT, (error)=>{
     
 });
 
-app.get("/",(req,res)=>{
-    res.send("Hello There. Enjoy the backend development!!!!");
-})
+
+
+const getData=async(req,res)=>{
+          
+         const post_Schema=new mongoose.Schema({text:String})
+         mongoose.models = {};
+         const post_Model=mongoose.model("Post",post_Schema)
+        const all_post= await post_Model.find({});
+          if(!all_post)
+          {
+            console.log("Something went wrong");
+          }
+          res.status(200).json({
+            message:"Data fetched succesful",
+            all_post
+          });
+          console.log("Success")
+    
+            
+        } 
+        
+
+    app.get("/",(req,res)=>{
+        res.send("Welcome to the backend developement")
+    })
+    app.get("/Post",getData)
